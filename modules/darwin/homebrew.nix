ee9c6f5566ef75711fb202456e99f8033419c10f
Martín Zamorano <martin@mzamorano.com>
2026-05-26 17:54:03 -0700

    darwin: add some homebrew env vars

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,12 +1,22 @@
 { inputs, config, ... }:
 
+let
+  homebrewEnv = {
+    HOMEBREW_NO_ANALYTICS = "1";
+    HOMEBREW_NO_ENV_HINTS = "1";
+    HOMEBREW_NO_UPDATE_REPORT_NEW = "1";
+  };
+in
 {
   homebrew = {
     enable = true;
 
     taps = builtins.attrNames config.nix-homebrew.taps;
 
-    onActivation.cleanup = "uninstall";
+    onActivation = {
+      cleanup = "uninstall";
+      extraEnv = homebrewEnv;
+    };
     global.autoUpdate = false;
   };
 
@@ -21,4 +31,6 @@
     };
     mutableTaps = false;
   };
+
+  environment.variables = homebrewEnv;
 }
