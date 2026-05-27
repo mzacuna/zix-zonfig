b84ff46b8aa3eb9049fa82876055467ec81b7c05
Martín Zamorano <martin@mzamorano.com>
2026-05-26 17:50:56 -0700

    linux: remove redundant bubblewrap (Steam dependency)

--- modules/linux/steam.nix
+++ modules/linux/steam.nix
@@ -1,11 +1,5 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, lib, ... }:
 
 lib.mkIf config.isGaming {
   programs.steam.enable = true;
-  environment.systemPackages = [ pkgs.bubblewrap ];
 }
