ae6fbd4bccf3dd45ff1b10859002301348df2544
Martín Zamorano <martin@mzamorano.com>
2026-05-29 23:42:01 -0700

    chore: add words to .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "Apheleia Eglot Elpaca Vertico agenix basedpyright cachix capf defvar dolist elpa elpaca flyspell jdtls jimeh lsp modified multimarkdown nixd pyright ryantm subdir treesit wgrep zhaofengli"))))
+((nil . ((jinx-dir-local-words . "Apheleia Eglot Elpaca Kuntem Orderless Vertico agenix basedpyright cachix capf defvar dolist elpa elpaca flyspell hm jdtls jimeh keylayout lsp modified multimarkdown nixd pyright ryantm subdir treesit wgrep zhaofengli"))))
