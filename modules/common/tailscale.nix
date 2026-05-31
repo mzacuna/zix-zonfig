acbbfea3595f5b0ac26f0ae2a99159dd59abe210
Martín Zamorano <martin@mzamorano.com>
2026-05-30 22:20:31 -0700

    refactor(common): change style of tailscale.nix

--- modules/common/tailscale.nix
+++ modules/common/tailscale.nix
@@ -6,47 +6,44 @@
 }:
 
 let
-  inherit (config) hostname username;
+  inherit (config)
+    flags
+    hostname
+    hosts
+    username
+    ;
+  inherit (lib.attrsets) attrValues genAttrs optionalAttrs;
+  inherit (lib.lists) optionals remove singleton;
+  inherit (lib.trivial) flip;
 
   tailnetIdentityFile = "~/.ssh/id_ed25519_tailnet";
 
   # Use ghostty-bin in Darwin because we use the cask.
-  ghosttyTerminfo =
-    if config.flags.system.darwin then pkgs.ghostty-bin.terminfo else pkgs.ghostty.terminfo;
+  ghosttyTerminfo = if flags.system.darwin then pkgs.ghostty-bin.terminfo else pkgs.ghostty.terminfo;
 
-  tailnetHosts = [
-    "acheron"
-    "nile"
-    "tigris"
-  ];
-
-  tailnetUserKeys = {
-    acheron = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF3zteOi7/zlCxo1xKd63Tvwh2K2ZJ38eMdWu4SI1R9J";
-    nile = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWTxI3UJ03CCA0SqRSHNJmKULo0ApOQIPwzpUWWLKjy";
-  };
-
-  peerHosts = lib.remove hostname tailnetHosts;
-  peerUserKeys = lib.attrValues (removeAttrs tailnetUserKeys [ hostname ]);
+  peerHosts = remove hostname hosts;
+  peerKeys =
+    {
+      acheron = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF3zteOi7/zlCxo1xKd63Tvwh2K2ZJ38eMdWu4SI1R9J";
+      nile = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWTxI3UJ03CCA0SqRSHNJmKULo0ApOQIPwzpUWWLKjy";
+    }
+    |> flip removeAttrs [ hostname ]
+    |> attrValues;
 in
 {
   # Install Ghostty's terminfo on targets.
-  environment.systemPackages = lib.optionals config.flags.tailnet.ssh.target [
-    ghosttyTerminfo
-  ];
+  environment.systemPackages = optionals flags.tailnet.ssh.target [ ghosttyTerminfo ];
 
-  users.users.${username}.openssh.authorizedKeys.keys =
-    lib.optionals config.flags.tailnet.ssh.target peerUserKeys;
+  users.users.${username}.openssh.authorizedKeys.keys = optionals flags.tailnet.ssh.target peerKeys;
 
-  home-manager.sharedModules = [
-    {
-      programs.ssh.settings = lib.optionalAttrs config.flags.tailnet.ssh.client (
-        lib.genAttrs peerHosts (host: {
-          HostName = host;
-          User = username;
-          IdentityFile = tailnetIdentityFile;
-          IdentitiesOnly = true;
-        })
-      );
-    }
-  ];
+  home-manager.sharedModules = singleton {
+    programs.ssh.settings =
+      genAttrs peerHosts (host: {
+        HostName = host;
+        User = username;
+        IdentityFile = tailnetIdentityFile;
+        IdentitiesOnly = true;
+      })
+      |> optionalAttrs flags.tailnet.ssh.client;
+  };
 }
