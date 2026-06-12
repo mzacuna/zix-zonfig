650f938ef62c746d5416345fe5f72b54d3f66121
Martín Zamorano <martin@mzamorano.com>
2026-06-12 08:53:46 -0700

    fix(darwin): set terminfo dirs for ssh sessions

--- modules/darwin/ssh.nix
+++ modules/darwin/ssh.nix
@@ -1,9 +1,23 @@
-{ ... }:
+{ config, lib, ... }:
 
+let
+  inherit (lib.strings) concatStringsSep;
+
+  terminfoDirs = [
+    "${config.system.path}/share/terminfo"
+    "/usr/share/terminfo"
+  ];
+in
 {
   services.openssh = {
     enable = true;
+
     extraConfig = ''
+      # zsh initializes terminfo before nix-darwin's /etc/zshenv finishes.
+      # Put Nix and macOS terminfo in sshd's child environment so TERM is known
+      # as soon as the login shell starts.
+      SetEnv TERMINFO_DIRS=${concatStringsSep ":" terminfoDirs}
+
       KbdInteractiveAuthentication no
       PasswordAuthentication no
       PermitRootLogin no
