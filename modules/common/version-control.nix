4351892a0735fa2abd35ba7760695a99b246ab30
Martín Zamorano <martin@mzamorano.com>
2026-06-18 16:25:52 -0700

    feat(common): add gh

--- modules/common/version-control.nix
+++ modules/common/version-control.nix
@@ -1,62 +1,70 @@
 {
   config,
   lib,
+  pkgs,
   username,
   ...
 }:
 
+let
+  inherit (lib.lists) singleton;
+in
 lib.mkIf config.flags.profiles.interactive {
-  home-manager.users.${username}.programs.git = {
-    enable = true;
+  home-manager.users.${username} = {
+    home.packages = singleton pkgs.gh;
 
-    settings = {
-      user = {
-        name = "Martín Zamorano";
-        email = "martin@mzamorano.com";
+    programs.git = {
+      enable = true;
+
+      settings = {
+        user = {
+          name = "Martín Zamorano";
+          email = "martin@mzamorano.com";
+        };
+
+        init.defaultBranch = "main";
+        color.ui = "auto";
+        pull.rebase = false;
+
+        alias = {
+          "a" = "add";
+          "aa" = "add -A";
+          "s" = "status";
+          "d" = "diff";
+          "co" = "checkout";
+          "br" = "branch";
+          "cf" = "commit"; # commit full; as in, a full message
+          "cc" = "commit -m"; # commit concise
+          "unstage" = "restore --staged";
+          "unstage-all" = "restore --staged :/";
+          "last" = "log -1 HEAD";
+          "lg" =
+            "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        };
       };
 
-      init.defaultBranch = "main";
-      color.ui = "auto";
-      pull.rebase = false;
-
-      alias = {
-        "a" = "add";
-        "aa" = "add -A";
-        "s" = "status";
-        "d" = "diff";
-        "co" = "checkout";
-        "br" = "branch";
-        "cf" = "commit"; # commit full; as in, a full message
-        "cc" = "commit -m"; # commit concise
-        "unstage" = "restore --staged";
-        "unstage-all" = "restore --staged :/";
-        "last" = "log -1 HEAD";
-        "lg" =
-          "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      };
+      ignores = [
+        ".idea"
+        ".direnv"
+        ".envrc"
+        ".env"
+        "*~"
+        ".fuse_hidden*"
+        ".directory"
+        ".Trash-*"
+        ".nfs*"
+        "nohup.out"
+        ".DS_Store"
+        ".AppleDouble"
+        ".LSOverride"
+        "Icon"
+        "._*"
+        "Thumbs.db"
+        ".Spotlight-V100"
+        ".Trashes"
+        ".agent-work/"
+        ".claude/settings.local.json"
+      ];
     };
-
-    ignores = [
-      ".idea"
-      ".direnv"
-      ".envrc"
-      ".env"
-      "*~"
-      ".fuse_hidden*"
-      ".directory"
-      ".Trash-*"
-      ".nfs*"
-      "nohup.out"
-      ".DS_Store"
-      ".AppleDouble"
-      ".LSOverride"
-      "Icon"
-      "._*"
-      "Thumbs.db"
-      ".Spotlight-V100"
-      ".Trashes"
-      ".agent-work/"
-      ".claude/settings.local.json"
-    ];
   };
 }
