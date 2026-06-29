470e4b12a331c11e49b3478aadc43f065c9ae6fa
Martín Zamorano <martin@mzamorano.com>
2026-06-29 08:21:22 -0700

    fix(acheron): restore G435 playback volume at login

--- modules/linux/pipewire.nix
+++ modules/linux/pipewire.nix
@@ -1,5 +1,13 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
+let
+  inherit (lib.meta) getExe';
+in
 lib.mkIf config.flags.profiles.graphical {
   security.rtkit.enable = true;
   services.pipewire = {
@@ -7,5 +15,21 @@ lib.mkIf config.flags.profiles.graphical {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+  };
+
+  systemd.user.services.g435-playback-volume = lib.mkIf (config.hostname == "acheron") {
+    description = "Set Logitech G435 hardware playback volume";
+    after = [
+      "pipewire.service"
+      "wireplumber.service"
+    ];
+    wantedBy = [ "default.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${getExe' pkgs.alsa-utils "amixer"} -c Headset sset 'G435 Wireless Gaming Headset Playback Volum' 175";
+      Restart = "on-failure";
+      RestartSec = "2s";
+    };
   };
 }
